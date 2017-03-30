628d7907ee1110a1f7877d8cd5229ed62f9f1c5e
JuJuBoSc <me@jujubosc.com>
2017-03-30 02:37:30 +0200

    Smoother navigation, handle debate and puzzle quests

--- Scripts/quester/logics/quest_completor.py
+++ Scripts/quester/logics/quest_completor.py
@@ -4,12 +4,16 @@ from roplus.helpers import nav
 from roplus.helpers import entities
 from roplus.helpers.questing import QuestGoal
 
+import Math
+import gameglobal
 import BigWorld
 import commQuest
 import const
 from guis import uiUtils
 from data import quest_data as QuestData
+from data import quest_npc_data as QuestNpcData
 from data import seeker_data as SeekerData
+from data import puzzle_data as PuzzleData
 
 import time
 
@@ -31,6 +35,7 @@ class GenericQuestCompletor(object):
         self.skipQuestGoal = False
         self.monstersToAttack = []
         self.markerNpcsToUse = []
+        self.debateNpcsToUse = []
         self.pendingMoveToEntity = None
         self.pendingMoveToEntityDistance = 1
 
@@ -39,6 +44,9 @@ class GenericQuestCompletor(object):
 
     def addMarkerNpcToUse(self, markerNpc):
         self.markerNpcsToUse.insert(0, markerNpc)
+
+    def addDebateNpcTouse(self, debateNpc):
+        self.debateNpcsToUse.insert(0, debateNpc)
 
     def moveToEntity(self, entity, distance=1.0):
         p = BigWorld.player()
@@ -64,7 +72,7 @@ class GenericQuestCompletor(object):
         if p:
             for taskGoal in p._genQuestGoal(self.questId):
                 results.append(QuestGoal(taskGoal))
-        return results        
+        return results
 
     def run(self):
         p = BigWorld.player()
@@ -82,6 +90,10 @@ class GenericQuestCompletor(object):
                 if not mainQuestGoal:
                     mainQuestGoal = questGoal
 
+        # Attempt to solve a puzzle if any
+        if self.processPuzzle():
+            return True
+
         # First kill monsters from quest goals
         if self.runAttackMonsters(self.monstersToAttack):
             return True
@@ -92,6 +104,10 @@ class GenericQuestCompletor(object):
 
         # Attempt to kill monsters that belong to player
         if self.runAttackMonsters(filter(lambda unit: unit.inCombat and unit.belongName == p.roleName, entities.getAttackableEntities(40))):
+            return True
+
+        # Attempt to use debate npcs from quest goals
+        if self.runDebateNpcs(self.debateNpcsToUse):
             return True
 
         # Attempt to use marker npcs from quest goals
@@ -110,9 +126,12 @@ class GenericQuestCompletor(object):
             return True
 
         # If any quest goal task is available, run it
-        if mainQuestGoal and not self.skipAutoQuest:
+        if mainQuestGoal and mainQuestGoal.trackSeekId and not self.skipAutoQuest:
+            trackDestination = nav.getDestinationBySeekId(mainQuestGoal.trackSeekId)
             self.state = "Run automatic task : " + str(mainQuestGoal.trackSeekId)
-            if time.time() - self.lastAutoPathTime > 2:
+            if trackDestination and p.position.distTo(Math.Vector3(trackDestination[0], trackDestination[1], trackDestination[2])) > 3:
+                nav.moveToPathFind(trackDestination)
+            elif time.time() - self.lastAutoPathTime > 2:
                 uiUtils.findPosById(mainQuestGoal.trackSeekId)
                 self.lastAutoPathTime = time.time()
             return True
@@ -149,6 +168,33 @@ class GenericQuestCompletor(object):
                     nav.moveToEntityPathFind(markerNpcToUse)
             return True
 
+    def runDebateNpcs(self, npcs):
+        if any(npcs):
+            p = BigWorld.player()
+            debateNpcToUse = sorted(npcs, key=lambda npc: p.position.distTo(npc.position))[0]
+            if p.position.distTo(debateNpcToUse.position) < 3:
+                self.state = "Use debate npc : " + debateNpcToUse.roleName
+                p.cell.onQuestDebate(self.questId, 1)
+                self.bot.engine.wait(1)
+            else:
+                self.state = "Approach to use : " + debateNpcToUse.roleName
+                if time.time() - self.lastAutoPathTime > 2:
+                    nav.moveToEntityPathFind(debateNpcToUse)
+            return True
+
+    def processPuzzle(self):
+        p = BigWorld.player()
+        puzzleProxy = gameglobal.rds.ui.puzzle
+        if puzzleProxy and puzzleProxy.puzzleId and puzzleProxy.questId == self.questId and puzzleProxy.puzzleInfo:
+            puzzleData = PuzzleData.data.get(puzzleProxy.puzzleId, {})
+            rightAnswer = puzzleData.get("rightAnswer", None)
+            if rightAnswer is not None:
+                roplus.log("Found a quest puzzle, answer index : " + str(rightAnswer))
+                p.cell.onQuestPuzzle(puzzleProxy.questId, puzzleProxy.puzzleId, rightAnswer)
+                self.bot.engine.wait(2)
+                return True
+
+
     def processQuestGoal(self, goal):
         p = BigWorld.player()
         if goal.order == "compItemCollect":
@@ -177,11 +223,18 @@ class GenericQuestCompletor(object):
                 if npc:
                     self.addMarkerNpcToUse(npc)
 
+        if goal.order == "debateNpc":
+            if goal.trackSeekData.get("type", "") == "npc":
+                npcId = goal.trackSeekData.get("npcId", 0)
+                npc = entities.findEntityByNpcId(npcId, maxRange=30)
+                if npc:
+                    self.addDebateNpcTouse(npc)
+
     def runTurnInQuest(self):
         p = BigWorld.player()
         completeNpcId = self.getCompleteNpcId()
         completeNpcTask = self.getCompleteNpcTask()
-        
+
         if completeNpcId and completeNpcTask:
             completeNpc = entities.findEntityByNpcId(completeNpcId)
             if completeNpc and p.position.distTo(completeNpc.position) < 3:
@@ -191,7 +244,10 @@ class GenericQuestCompletor(object):
                 self.bot.engine.wait(2)
             else:
                 self.state = "Going to turn in quest ..."
-                if time.time() - self.lastAutoPathTime > 2:
+                seekDestination = nav.getDestinationBySeekId(completeNpcTask)
+                if seekDestination and p.position.distTo(Math.Vector3(seekDestination[0], seekDestination[1], seekDestination[2])) > 3:
+                    nav.moveToPathFind(seekDestination)
+                elif time.time() - self.lastAutoPathTime > 2:
                     uiUtils.findPosById(completeNpcTask)
                     self.lastAutoPathTime = time.time()
 
