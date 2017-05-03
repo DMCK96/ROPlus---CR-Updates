bae036cda391356d3d80a8bae27ce542b063c072
DMCK96 <d.mckibbin@2016.LJMU.ac.uk>
2017-05-03 09:06:42 +0100

    Blademaster CR Update
    Updated for up to level 20. Increased distance usable as skills will
    still work from the greater distance. Added more skills and conditions,
    still working on making continuous skills attack in succession.

--- Combats/Blademaster.py
+++ Combats/Blademaster.py
@@ -20,6 +20,9 @@ class Combat(CombatBase):
         self.QuickSlash = Skill(1301)
         self.MercilessStrike = Skill(1304)
         self.JuggernautSweep = Skill(1313)
+        self.Skypiercer = Skill(1315)
+        self.ShadowBreaker = Skill(1316)
+        self.Retribution = Skill(1345)
         self.MartialInstinctSecret = Skill(1303)
 
     # Called in loop during the combat
@@ -30,7 +33,7 @@ class Combat(CombatBase):
             # Move close to target if needed
             if self.handleMove:
                 tDistance = p.position.distTo(target.position)
-                if tDistance > 3:
+                if tDistance > 5:
                     nav.moveToPathFind((target.position.x, target.position.y, target.position.z, p.mapID))
                     return
                 else:
@@ -44,18 +47,26 @@ class Combat(CombatBase):
             if self.handleMove:
                 p.faceTo(target)
 
-            if self.MartialInstinctSecret.isKnown() and p.position.distTo(target.position) <= 3.0 and self.MartialInstinctSecret.isUsable():
+            if self.ShadowBreaker.isKnown() and p.position.distTo(target.position) <= 5.0 and self.ShadowBreaker.isUsable() and not self.ShadowBreaker.isOnCooldown():
+                self.ShadowBreaker.cast(target)
+                return
+
+            if self.MartialInstinctSecret.isKnown() and p.position.distTo(target.position) <= 5.0 and self.MartialInstinctSecret.isUsable() and not self.MartialInstinctSecret.isOnCooldown() and not p.hasState(30804):
                 self.MartialInstinctSecret.cast(target)
                 return
 
-            if self.MercilessStrike.isKnown() and p.position.distTo(target.position) <= 3.0 and self.MercilessStrike.isUsable():
+            if self.MercilessStrike.isKnown() and p.position.distTo(target.position) <= 5.0 and self.MercilessStrike.isUsable() and self.Skypiercer.isOnCooldown():
                 self.MercilessStrike.cast(target)
                 return
 
-            if self.QuickSlash.isKnown() and p.position.distTo(target.position) <= 3.0 and self.QuickSlash.isUsable():
+            if self.Skypiercer.isKnown() and p.position.distTo(target.position) <= 5.0 and self.Skypiercer.isUsable():
+                self.Skypiercer.cast(target)
+                return
+
+            if self.QuickSlash.isKnown() and p.position.distTo(target.position) <= 5.0 and self.QuickSlash.isUsable() and self.MercilessStrike.isOnCooldown() and self.JuggernautSweep.isOnCooldown() and self.MartialInstinctSecret.isOnCooldown() and self.ShadowBreaker.isOnCooldown():
                 self.QuickSlash.cast(target)
                 return
 
-            if self.JuggernautSweep.isKnown() and p.position.distTo(target.position) <= 3.0 and self.JuggernautSweep.isUsable():
+            if self.JuggernautSweep.isKnown() and p.position.distTo(target.position) <= 5.0 and self.JuggernautSweep.isUsable():
                 self.JuggernautSweep.cast(target)
                 return
